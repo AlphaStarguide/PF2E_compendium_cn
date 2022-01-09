57195bbf7afad4ad70cca8bdae773df2fed66616
Kuroni <90250769+AlphaStarguide@users.noreply.github.com>
2022-01-09 18:09:23 +0900

    Update babele.js

--- babele.js
+++ babele.js
@@ -1,7 +1,7 @@
 Hooks.on('init', () => {
     if (typeof Babele !== 'undefined') {
         Babele.get().register({
-            module: 'pf2_compendium_cn',
+            module: 'PF2E_compendium_cn',
             lang: 'cn',
             dir: 'compendium'
         });
